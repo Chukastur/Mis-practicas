7e14abcf8f9920aa30eee5cb85e86578346a1052
Chukastur <fernandolcmusic@gmail.com>
2025-08-27 02:19:12 +0200

    Remove hardcoded API key from getImagen2
    Removed API key from getImagen2 function for security.

--- cursoreact/02-intro-javascript/11-async-await.js
+++ cursoreact/02-intro-javascript/11-async-await.js
@@ -21,7 +21,7 @@ getImagen().then (console.log);
 const getImagen2 = async () => {
     try {
 
-        const apiKey = '5oQ1SpAbmMvbdMixCbCZFMjxMngUCCdw';
+        const apiKey = '';
         const resp = await fetch (`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
         const {data} = await resp.json (); 
         const {url} = data.images.original;
